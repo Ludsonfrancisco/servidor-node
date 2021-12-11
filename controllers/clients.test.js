bc21f5edfb334eed6ed7383ed7b01e6ad9ac2257
Ludson Francisco <ludsonfrancisco1010@gmail.com>
2021-12-10 22:35:57 -0300

    update test controllers

--- controllers/clients.test.js
+++ controllers/clients.test.js
@@ -3,12 +3,21 @@ const expect = require('chai').expect
 const sinon = require('sinon')
 
 describe('clients controller', () => {
-  it(' returns all clients', () => {
+  it(' creat all clients', () => {
     let res = {
       send: function () { }
     }
     let mock = sinon.mock(res)
     mock.expects('send').once().withArgs('CLIENTS/NEW')
     clientsControllers.creat({}, res)
+  })
+
+  it(' returns all clients', () => {
+    let res = {
+      send: function () { }
+    }
+    let mock = sinon.mock(res)
+    mock.expects('send').once().withArgs('CLIENTS')
+    clientsControllers.clients({}, res)
   })
 })
